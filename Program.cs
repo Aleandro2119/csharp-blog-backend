b88294f48ca556a76517d3fa9668fae14f06e1f6
Aleandro2119 <aleandro.basilico@gmail.com>
2022-06-21 14:48:35 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using csharp_blog_backend.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+// MODIFICA AGGIUNTA
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+        });
+});
+// Add services to the container.
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BlogContext>(options => options.UseSqlServer(connectionString));
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BlogContext>(opt =>
+    opt.UseInMemoryDatabase("posts"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors();
+app.UseStaticFiles();
+
+
+app.MapControllers();
+//inizializzazione db
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<BlogContext>();
+    context.Database.EnsureCreated();
+
+}
+
+app.Run();
